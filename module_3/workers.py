00abd35774d50982f544f1d9742d1da3a1c7da72
Дмитрий Курбатов <kurbatovdv@yandex.ru>
2020-10-29 03:19:20 +1000

    Initial commit

--- module_3/workers.py
+++ module_3/workers.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 def make_price(url_ta):
-    url = 'https://www.tripadvisor.ru' + url_ta
-    result = {'URL_TA': url_ta, 'Price Range': "Nan", "Cuisine Style": "Nan", 'Number of Reviews': "Nan"}
+    url = 'https://www.tripadvisor.com' + url_ta
+    result = {'URL_TA': url_ta, 'Price Range': "nan", "Cuisine Style": "nan", 'Number of Reviews': "nan"}
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
     rest_data = [d.text for d in soup.find_all('a', class_="_2mn01bsa")]
     reviews_count = soup.find('span', class_="_3Wub8auF")
     if reviews_count:
         reviews_count = reviews_count.text
-        result['Number of Reviews'] = reviews_count[:reviews_count.rfind(" ")].replace(" ", "")
+        result['Number of Reviews'] = int(reviews_count[:reviews_count.rfind(" ")].replace("\xa0", "").replace(",",""))
     if len(rest_data) > 1:
         if rest_data[0] in ['$','$$ - $$$', '$$$$']:
             result['Price Range'] = rest_data[0]
